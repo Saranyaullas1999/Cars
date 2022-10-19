876f2b53abdb7ceaffd8c70643c47435aeddddec
Saranyaullas1999 <saranyaullas1999@gmail.com>
2022-10-20 03:40:18 +0530

    addcar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,15 +4,27 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './navbar/navbar.component';
+import { AddcarsComponent } from './addcars/addcars.component';
+import { RouterModule,Routes } from '@angular/router';
+import { FormsModule } from '@angular/forms';
 
+
+const appRoutes:Routes=[
+  {
+    path:"",component:AddcarsComponent
+  }
+]
 @NgModule({
   declarations: [
     AppComponent,
-    NavbarComponent
+    NavbarComponent,
+    AddcarsComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    RouterModule.forRoot(appRoutes),
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
